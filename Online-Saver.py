04c37debc21316694f9ee19e3a827708d6a729b9
Syntaxxx <67672258+1Syntaxxx1@users.noreply.github.com>
2020-09-17 22:54:05 +0300

    Update Online-Saver.py

--- Online-Saver.py
+++ Online-Saver.py
@@ -16,7 +16,7 @@ print('''
 
 url = input("Enter file url: ")
 
-print("\nExamples:  \n>> pdf.\n>> jpeg or jpg.\n>> png.\n>> doc.\n>>mp4/3.\n=> etc...")
+print("\nExamples:  \n>> pdf.\n>> jpeg or jpg.\n>> png.\n>> doc.\n>> mp4/3.\n>> txt.\n=> etc...")
 ext = input("what is the file extension? ")
 
 print("\nThe file will be present in the current directory.")
